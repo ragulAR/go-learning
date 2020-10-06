5e794e4e1a9bbf5533d7c9ae3e670486d79757d8
Ragul Ravindira <rahul1996@icloud.com>
2020-10-06 13:36:11 +0530

    Implemented Goroutines

---
+++ src/firstapp/Goroutines.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg = sync.WaitGroup{}
+var counter = 0
+var m = sync.RWMutex{}
+
+func main() {
+
+	//runtime.GOMAXPROCS(100) // avoid parallelism
+	for i := 0; i < 10; i++ {
+		wg.Add(2)
+		m.RLock()
+		go sayHello()
+		m.Lock()
+		go increment()
+	}
+	wg.Wait()
+}
+
+func sayHello() {
+	fmt.Printf("Hello! #%v\n", counter)
+	m.RUnlock()
+	wg.Done()
+}
+
+func increment() {
+	counter++
+	m.Unlock()
+	wg.Done()
+}
